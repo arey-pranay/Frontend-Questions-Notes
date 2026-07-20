87a97fd3e423c46a55bc92d3e46d3632f707be12
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-07-20 15:01:30 +0530

    Create index.ts

---
+++ GFE-3M/Functions/036. jquery class manipulation/index.ts
@@ -0,0 +1,43 @@
+interface JQuery {
+  toggleClass: (className: string, state?: boolean) => JQuery;
+  addClass: (className: string) => JQuery;
+  removeClass: (className: string) => JQuery;
+}
+
+function classNameTokenSet(className: string): Set<string> {
+  return new Set(className.trim().split(/\s+/));
+}
+
+export default function $(selector: string): JQuery {
+  const element = document.querySelector(selector);
+
+  return {
+    toggleClass: function (className: string, state?: boolean): JQuery {
+      // No-op if there is no matching element.
+      if (element == null) {
+        return this;
+      }
+
+      const classes = classNameTokenSet(className);
+      const elementClasses = classNameTokenSet(element.className);
+
+      classes.forEach((cls) => {
+        // `undefined` means "toggle"; otherwise `state` forces add/remove.
+        const shouldRemove =
+          state === undefined ? elementClasses.has(cls) : !state;
+        shouldRemove ? elementClasses.delete(cls) : elementClasses.add(cls);
+      });
+
+      element.className = Array.from(elementClasses).join(' ');
+      return this;
+    },
+    addClass: function (className: string): JQuery {
+      this.toggleClass(className, true);
+      return this;
+    },
+    removeClass: function (className): JQuery {
+      this.toggleClass(className, false);
+      return this;
+    },
+  };
+}
