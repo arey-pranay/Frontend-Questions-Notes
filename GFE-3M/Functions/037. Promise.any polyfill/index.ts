f6a33d7c1e0e049a5ba2fb0f83f70e1fc9b10bb6
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-07-21 13:19:54 +0530

    Implement Promise.any polyfill

---
+++ GFE-3M/Functions/037. Promise.any polyfill/index.ts
@@ -0,0 +1,24 @@
+export default function promiseAny<T>(iterable: Array<T>): Promise<T> {
+  const errors: Array<any> = new Array(iterable.length);
+  if (iterable.length === 0) {
+    return Promise.reject(new AggregateError(errors));
+  }
+  let ans: Promise<T>;
+  ans = new Promise(async (resolve, reject) => {
+    let rejectCount = 0;
+    iterable.forEach(async (item, index) => {
+      try {
+        const value = await item;
+        resolve(value);
+      } catch (err) {
+        errors[index] = err;
+        rejectCount++;
+
+        if (rejectCount === iterable.length) {
+          reject(new AggregateError(errors));
+        }
+      }
+    });
+  });
+  return ans;
+}
