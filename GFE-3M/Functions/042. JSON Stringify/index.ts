01ced18894d6ff1e8afd98252e557167726b19a0
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2026-08-04 17:30:16 +0530

    Create index.ts

---
+++ GFE-3M/Functions/042. JSON Stringify/index.ts
@@ -0,0 +1,18 @@
+export default function jsonStringify(value: unknown): string {
+  if (Array.isArray(value)) {
+    const arrayValues = value.map((item) => jsonStringify(item));
+    return `[${arrayValues.join(",")}]`;
+  }
+  if (typeof value === "object" && value !== null) {
+    const objectEntries = Object.entries(value).map(
+      ([k, v]) => `"${k}":${jsonStringify(v)}`,
+    );
+    return `{${objectEntries.join(",")}}`;
+  }
+
+  if(typeof value === "string"){
+    return `"${value}"`
+  }
+
+  return String(value);
+}
